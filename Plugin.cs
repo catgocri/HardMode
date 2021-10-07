282153c09a394770c1e80b7c06e400ec24df83ca
catgocri <91496885+catgocri@users.noreply.github.com>
2021-10-07 21:21:49 +0200

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -2,15 +2,15 @@ using BepInEx;
 using HarmonyLib;
 using UnityEngine;
 
-namespace catgocri.HardMode.PotionCraft
+namespace catgocrihxpmods.HardMode.PotionCraft
 {
-    [BepInPlugin("net.catgocri.PotionCraft.HardMode", "Potion Craft Hard Mode", "1.0")]
+    [BepInPlugin("net.catgocrihxpmods.PotionCraft.HardMode", "Potion Craft Hard Mode", "1.0")]
     public class TemplatePlugin : BaseUnityPlugin
     {   
       void Awake()
       {
         Debug.Log(@"[HardMode]: Loaded");
-        var harmony = new Harmony("net.catgocri.PotionCraft.Template");
+        var harmony = new Harmony("net.catgocrihxpmods.PotionCraft.Template");
         harmony.PatchAll();
       }
     }
