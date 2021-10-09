6a9908edc52e45d6270b0d10e04fc18c548d02fb
catgocri <91496885+catgocri@users.noreply.github.com>
2021-10-09 18:54:43 +0200

    Add files via upload

---
+++ GameHooks/GardenEvent.cs
@@ -0,0 +1,29 @@
+
+using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace catgocrihxpmods.HardMode.PotionCraft.GameHooks
+{
+    [HarmonyPatch(typeof(ObjectBased.Garden.GrowingSpot.SpotPlant), "GatherIngredient")]
+    public static class GardenEvent
+    {
+        public static EventHandler<GardenEventEventArgs> onGardenEvent;
+
+        static bool Prefix(GameObject __instance)
+        {
+            var e = new GardenEventEventArgs();
+            onGardenEvent?.Invoke(null, e);
+            if (e.ingredientAmount.HasValue)
+            {
+                Reflection.SetPrivateField(__instance, "ingredientAmount", e.ingredientAmount);
+            }
+            return true;
+        }
+    }
+
+    public class GardenEventEventArgs : EventArgs
+    {
+        public Vector2Int? ingredientAmount { get; set; }
+    }
+}
