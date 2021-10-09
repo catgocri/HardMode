2d9808a31ed69c709665d5437e1a1bbab11929b2
catgocri <91496885+catgocri@users.noreply.github.com>
2021-10-09 18:55:19 +0200

    Add files via upload

---
+++ HardModeModifiers/GardenModifier.cs
@@ -0,0 +1,26 @@
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using HarmonyLib;
+using catgocrihxpmods.HardMode.PotionCraft.GameHooks;
+
+namespace catgocrihxpmods.HardMode.PotionCraft
+{
+    public class GardenModifier : HardModeModifier
+    {
+        public new string name = "Garden";
+        public override void LoadFromBindings(ConfigFile config)
+        {
+            SetActive(config.Bind(name + " Settings", "doGardenModifier", true, "If the gardens harvest count should be modified.").Value);
+
+            if (this.active)
+            {
+                GardenEvent.onGardenEvent += (sender, e) =>
+                {
+                    // Sets to (1, 0)
+                    e.ingredientAmount = Vector2Int.right;
+                };
+            }
+        }
+    }
+}
